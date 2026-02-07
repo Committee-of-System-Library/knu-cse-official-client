b25f7b36f302aa8b8a13e1d381fbb21775c1ec04
Yujin Oh <yu05jin@naver.com>
2026-02-07 22:49:13 +0900

    Update Navbar.tsx
    사이드바 학생회눌렀을떄 내려오게 수정

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -92,11 +92,10 @@ export default function Navbar() {
 
       {/* Expanded Menu */}
       <div
-        className={`absolute left-0 w-full bg-white border-b border-[rgba(0,0,0,0.15)] px-36 pt-8 pb-10 -z-10 transition-all duration-200 ${
-          hoveredMenu
-            ? "opacity-100 translate-y-0 pointer-events-auto"
-            : "opacity-0 -translate-y-2 pointer-events-none"
-        }`}
+        className={`absolute left-0 w-full bg-white border-b border-[rgba(0,0,0,0.15)] px-36 pt-8 pb-10 -z-10 transition-all duration-200 ${hoveredMenu !== null
+          ? "opacity-100 translate-y-0 pointer-events-auto"
+          : "opacity-0 -translate-y-2 pointer-events-none"
+          }`}
       >
         <div className="flex gap-10 items-start">
           {menuData.map((section, index) => (
